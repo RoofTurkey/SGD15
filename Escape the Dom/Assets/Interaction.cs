efb250ff7a68737964f5ae2d8133725f3bcc25f7
Nirish Alakhramsing <Nirish.Alakhramsing@hva.nl>
2015-07-20 21:15:26 +0200

    Add more interaction scripts
    Script to put on objects so it can be highlighted

---
+++ Escape the Dom/Assets/Interaction.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Interaction : MonoBehaviour {
+
+	public float hitDistance = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	Vector3 fwd = transform.TransformDirection(Vector3.forward);
+
+	// Update is called once per frame
+	void Update () {
+	
+		//Nirish : Player now will know if an object is interactable if the raycast hits.
+		if (Physics.Raycast (transform.position, fwd, hitDistance)) {
+			print ("There is something in front of the object!");
+		} else {
+			print ("There is no interactable object infront of me!");
+		}
+
+	}
+
+
+
+}
---
+++ Escape the Dom/Assets/ObjHighlighting.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjHighlighting : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+			
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// Provides a highlight color to object thats being interacted with.
+	private Color startcolor;
+
+	void highlightObject(bool doestHit)
+	{
+		if (doestHit) {
+			startcolor = GetComponent<Renderer> ().material.color;
+			GetComponent<Renderer> ().material.color = Color.yellow;
+		} else {
+			GetComponent<Renderer>().material.color = startcolor;
+		}
+	}
+
+	void OnMouseExit()
+	{
+
+	}
+}
