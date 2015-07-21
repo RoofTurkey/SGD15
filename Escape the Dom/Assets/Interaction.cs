4d4217111cb2d4982dd2c957f6757a90997c9df2
Nirish Alakhramsing <Nirish.Alakhramsing@hva.nl>
2015-07-21 17:57:11 +0200

    add Recording
    Added recording script, and small addiotion to Interaction script. and a
    temp prefab

--- Escape the Dom/Assets/Interaction.cs
+++ Escape the Dom/Assets/Interaction.cs
@@ -6,35 +6,35 @@ public class Interaction : MonoBehaviour
 {
 	private const string _cameraRecording = "Recording";
 	private float _distance;
+	public bool canRecord;
 	public float maxDistance;
 
 	// Use this for initialization
 	public void Start () 
 	{ 
 		maxDistance = 6.0f;
+		canRecord = false;
 	}
 
 	// Update is called once per frame
 	public void Update () 
 	{
-		Vector3	fwd = transform.TransformDirection(Vector3.forward);
+		Vector3 fwd = transform.TransformDirection (Vector3.forward);
 		RaycastHit hit;
 		//Player now will know if an object is interactable if the raycast hits.
-		if (Physics.Raycast (transform.position, fwd, out hit, maxDistance)) 
-		{
-			if (hit.transform.tag == "InterActive") 
-			{
+		if (Physics.Raycast (transform.position, fwd, out hit, maxDistance)) {
+			if (hit.transform.tag == "InterActive") {
 				hit.transform.SendMessage ("CreateTextMessage", _cameraRecording);
+				hit.transform.SendMessage ("highlightObject", true);
 
-				hit.transform.SendMessage("highlightObject", true);
+				canRecord = true;
 			}
-		} 
-		else 
-		{
-			foreach (GameObject interActive in GameObject.FindGameObjectsWithTag("InterActive" ))
-			{
-				ObjHighlighting obj = interActive.GetComponent<ObjHighlighting>();
-				obj.highlightObject(false);
+		} else {
+			foreach (GameObject interActive in GameObject.FindGameObjectsWithTag("InterActive" )) {
+				ObjHighlighting obj = interActive.GetComponent<ObjHighlighting> ();
+				obj.highlightObject (false);
+
+				canRecord = false;
 			}
 		}
 	}
---
+++ Escape the Dom/Assets/Recording.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Recording : MonoBehaviour {
+
+	Interaction rec;
+
+	// Use this for initialization
+	void Start () {
+		rec = GameObject.Find ("HandHeldCam").GetComponent<Interaction> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown("r")){
+			print("I am recording");
+			StartRecording(rec.canRecord);
+		}
+	}
+
+
+
+	public void StartRecording(bool canRecord) {
+		print("insideRecordingMethod");
+		if (rec.canRecord) {
+
+			Application.CaptureScreenshot ("Assets/Resources/Screenshot.png");
+			print("khebt geprint");
+			string pathToImage = "Screenshot";
+			print (pathToImage);
+
+			Texture2D texture = Resources.Load(pathToImage)as Texture2D;
+
+			print (texture);
+			Renderer renderer = GetComponent<Renderer>();
+			renderer.material.mainTexture = texture;
+			//GetComponent<Renderer>().material.color = pathToImage;
+
+
+			rec.canRecord = false;
+		} else {
+
+		}
+	}
+
+
+
+}
