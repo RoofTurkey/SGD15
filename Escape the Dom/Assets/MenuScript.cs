878ee2e1d64b8b58a0453d5a142bccc73a2ccb50
Nirish Alakhramsing <Nirish.Alakhramsing@hva.nl>
2015-07-23 11:08:01 +0200

    Add w.i.p. Menu and Plugin iTween

---
+++ Escape the Dom/Assets/MenuScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public bool isQuit= false;
+	
+	
+	void OnMouseEnter(){
+		GetComponent<Renderer>().material.color = Color.red;
+	}
+	
+	void OnMouseExit(){
+		GetComponent<Renderer>().material.color = Color.white;
+	}
+	
+	void OnMouseUp(){
+		if (isQuit == true) {
+			Application.Quit ();
+		} else {
+			//go to main camera
+		}
+	}
+}
