3345bf78e056fb2e23af04942c885beb418d28f9
RoofTurkey <tim.kruijsen@gmail.com>
2015-07-24 11:51:06 +0200

    Endgame toegevoegd, radios toegevoegd en pilaren d'r in

---
+++ Escape the Dom/Assets/Scripts/EndGame.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class EndGame : MonoBehaviour {
+
+	GameObject score;
+	public Text winText;
+
+	void Start () {
+		score = GameObject.Find("Score");
+		winText.text = "";
+	}
+	
+	void OnTriggerEnter (Collider other) {
+		if (other.tag == "Player") {
+			score.GetComponent<Timer>().Finish();
+			winText.text = "Gefeliciteerd, je hebt het gehaald! Jou tijd was " + score.GetComponent<Timer>().timeDisplay.text + " en je hebt maar liefst " + score.GetComponent<ScoreCounter>().playerScore + " van de 5 informatiepunten gevonden en afgespeeld!";
+		}
+	}
+}
